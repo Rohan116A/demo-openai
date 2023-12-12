44129c57fe4015d9aa7ee1aee56a2f21856c80f9
Rohan116A <153647919+Rohan116A@users.noreply.github.com>
2023-12-12 22:06:26 +0530

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,18 +4,17 @@ export default function Hero() {
   return (
     <section>
       <div className='container'>
-        <div className="hero h-96 md:h-[500px] rounded-box overflow-hidden" style={{ backgroundImage: `url("https://placeimg.com/1000/800/tech")` }}>
+        <div className="hero h-96 md:h-[500px] rounded-box overflow-hidden" style={{ backgroundImage: `url("https://openai-chatgpt-premium.vercel.app/openai-chatgpt.jpeg")` }}>
           <div className="hero-overlay bg-opacity-60 bg-secondary"></div>
           <div className="hero-content text-center text-secondary-content">
             <div className="max-w-lg">
               <h1 className="mb-5 sm:mb-7 text-4xl sm:text-5xl font-bold">
-                Let's make your
-                product a success
+               OpenAI ChatGPT Premium MOD APK
               </h1>
               <p className="mb-5 sm:mb-7 sm:text-lg">
-                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.
+                Download and Install OpenAI ChatGPT Premium MOD APK Free. Use all Premium Features on Android Smartphone.
               </p>
-              <button className="btn btn-warning sm:btn-wide">Get Started</button>
+              <button className="btn btn-warning sm:btn-wide">Download APK</button>
             </div>
           </div>
         </div>
