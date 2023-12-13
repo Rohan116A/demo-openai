e8cd4f5426a582274ea8376cc32b9e88a181f237
Rohan116A <153647919+Rohan116A@users.noreply.github.com>
2023-12-13 21:39:32 +0530

    Update Article.js

--- src/components/Article.js
+++ src/components/Article.js
@@ -9,29 +9,29 @@ export default function Article()  {
         </div>
 
     <article>
-<p>
+<p className='text-lg sm:text-2xl mb-6 md:mb-14'>
   ChatGPT Premium MOD APK is Safe to install and use on your Phone. We tested
   multiple devices and also verified by VirulTotal. So, you only need to
   download the APK and then install it on your Android phone. To install it
   successfully, you definitely need to go through some necessary steps.
 </p>
-<p>
+<p className='text-lg sm:text-2xl mb-6 md:mb-14'>
   Step 1: Open the browser [example: Chrome, Firefox, Safari] and visit this
   site.
 </p>
-<p>
+<p className='text-lg sm:text-2xl mb-6 md:mb-14'>
   Step 2: Tap on the download button on this page and wait for the downloading.
 </p>
-<p>
+<p className='text-lg sm:text-2xl mb-6 md:mb-14'>
   Step 3: Once it is finished, go to the folder where the APK is saved on the
   device <b>File Manager</b>.
 </p>
-<p>Step 4: Enable the <b>Unknown Sources</b> from the <b>Setting</b>.</p>
-<p>
+<p className='text-lg sm:text-2xl mb-6 md:mb-14'>Step 4: Enable the <b>Unknown Sources</b> from the <b>Setting</b>.</p>
+<p className='text-lg sm:text-2xl mb-6 md:mb-14'>
   Step 5: You get to see the <b>Install</b> button. Tap on it and wait a few
   seconds.
 </p>
-<p>
+<p className='text-lg sm:text-2xl mb-6 md:mb-14'>
   Congratulations! Now you are ready to use this application with all premium
   features on your phone.
 </p>
