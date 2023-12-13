cc9adb033f3c7463df30201b0e86e2e9481c7a8f
Rohan116A <153647919+Rohan116A@users.noreply.github.com>
2023-12-13 21:55:28 +0530

    Update Article.js

--- src/components/Article.js
+++ src/components/Article.js
@@ -40,26 +40,23 @@ export default function Article()  {
 
     <div class="carousel carousel-center rounded-box gap-1">
     <div class="carousel-item">
-    <img src="https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg" class="rounded-box" />
+    <img src="https://play-lh.googleusercontent.com/73YdqpsZPOjPdiDrURm2whbE-CAoIvPLPSpoH76y4vDz-K19JDIutwWBiHWY-1SzACs=w526-h296-rw" class="rounded-box" />
   </div> 
   <div class="carousel-item">
-    <img src="https://daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg" class="rounded-box" />
+    <img src="https://play-lh.googleusercontent.com/bw67GNVLWhLKv8taCwdyttVzmI0R-NVHOZ6ms33Cpz1UVNn9ZAR_B2E_PbtIeouKDoQ=w526-h296-rw" class="rounded-box" />
   </div> 
   <div class="carousel-item">
-    <img src="https://daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg" class="rounded-box" />
+    <img src="https://play-lh.googleusercontent.com/pfSjG93dd5Xy2DBh4FsHJPFNWz5sXB1Dwg_qPzeXXw_P7Mq6oJxQz8FrdW1dEj8K6IXN=w526-h296-rw" class="rounded-box" />
   </div> 
   <div class="carousel-item">
-    <img src="https://daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg" class="rounded-box" />
+    <img src="https://play-lh.googleusercontent.com/8Z4_BoVmJkSEftmXGpW3Fzdstq9DZKO55BrOfDhpOuPTV3Hdm5ADdTAQ3LIKmE9uwUj6=w526-h296-rw" class="rounded-box" />
   </div> 
   <div class="carousel-item">
-    <img src="https://daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg" class="rounded-box" />
+    <img src="https://play-lh.googleusercontent.com/eXCnYvgm8DAyVaBTKTQoE-zhhvI1VcsuCunsEOtmZmjkFtrNoC1GqoXb4zeFbCJQgw=w526-h296-rw" class="rounded-box" />
   </div> 
   <div class="carousel-item">
-    <img src="https://daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg" class="rounded-box" />
+    <img src="https://play-lh.googleusercontent.com/jYSdAP5tqMrku4A9P3bHbqG7vnuQOtpAfzMJ_y7sGDusu4Bd-myy0A5eOZYYj3D73sc=w526-h296-rw" class="rounded-box" />
   </div> 
-  <div class="carousel-item">
-    <img src="https://daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" class="rounded-box" />
-  </div>
 </div>
          
         </div>
