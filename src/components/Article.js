7b713e7c49ce62408288a59861b83728539d98c5
Rohan116A <153647919+Rohan116A@users.noreply.github.com>
2023-12-13 20:30:44 +0530

    Update Article.js

--- src/components/Article.js
+++ src/components/Article.js
@@ -6,10 +6,11 @@ export default function Article()  {
       <div className='container'>
         <div className='text-center'>
           <h2 className='text-3xl sm:text-5xl font-bold mb-4'>OpenAI ChatGPT Premium Features</h2>
-          <p className='text-lg sm:text-2xl mb-6 md:mb-14'>
-            Here we share all pro features of ChatGPT MOD APK for Android. Take a look and then Install on your smartphone.
-          </p>
         </div>
+
+    <article class="mx-auto max-w-6xl">
+<p>Hello World</p>
+    </article>
 
          
         </div>
