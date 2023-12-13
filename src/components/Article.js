5c90105823a5a84a46c6c51b0b8a8368fefa67f6
Rohan116A <153647919+Rohan116A@users.noreply.github.com>
2023-12-13 20:29:14 +0530

    Update Article.js

--- src/components/Article.js
+++ src/components/Article.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 export default function Article()  {
   return (
-    <section className='py-10 md:py-16 bg-neutral'>
-        <div className='container'>
-          <div className='text-center text-white'>
-            <h2 className='text-3xl sm:text-5xl font-bold mb-4'>We have some fans.</h2>
-            <p className='text-lg sm:text-2xl mb-6 md:mb-14'>
-              We create engaging experiences that are innovatingand beautiful.
-            </p>
-          </div>
+    <section className='py-10 md:py-16'>
+      <div className='container'>
+        <div className='text-center'>
+          <h2 className='text-3xl sm:text-5xl font-bold mb-4'>OpenAI ChatGPT Premium Features</h2>
+          <p className='text-lg sm:text-2xl mb-6 md:mb-14'>
+            Here we share all pro features of ChatGPT MOD APK for Android. Take a look and then Install on your smartphone.
+          </p>
+        </div>
 
          
         </div>
