c438b4c8130db181de718a2409f9549d81f4ba59
Rohan116A <153647919+Rohan116A@users.noreply.github.com>
2023-12-13 21:45:36 +0530

    Update CTA.js

--- src/components/CTA.js
+++ src/components/CTA.js
@@ -6,7 +6,7 @@ export default function CTA() {
       <div className='container'>
         <div className='flex flex-col md:flex-row justify-center items-center gap-6 text-center md:text-start'>
           <span className='flex-grow text-4xl md:text-5xl 2xl:text-6xl font-bold text-primary'>Let's Download and <br className='hidden sm:inline' /> Install on Android.</span>
-          <a href="https://apk.idealfollow.in/openai-chatgpt-premium-mod-apk/" rel="nofollow" target="_blank" id="download">><button className='btn btn-secondary rounded-full sm:btn-lg'>Download APK</button></a>
+          <a href="https://apk.idealfollow.in/openai-chatgpt-premium-mod-apk/" rel="nofollow" target="_blank" id="download"><button className='btn btn-secondary rounded-full sm:btn-lg'>Download APK</button></a>
         </div>
       </div>
     </section>
